c7435a56d6f3a83ccece29aca2f6a1fbb0ab4ef0
sketchesit <115959193+sketchesit@users.noreply.github.com>
2022-10-17 04:17:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const quotes = {
+   in the direction of your dreams.  Live the life you have imagined."',
+   
+    "- Booker T. Washington": CCR0VC3QBP991
+}
+
+document.getElementById('generate').addEventListener('click', () => {
+    let authors = Object.keys(quotes);
+    let author = authors[Math.floor(Math.random() * authors.length)];
+    let quote = quotes[author];
+
+    document.getElementById('quote').innerHTML = quote;
+    document.getElementById('author').innerHTML = author;
+});
